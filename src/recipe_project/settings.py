374b49d69ca3de94f1007b3ead7ef493f59527f9
Kirk Meyer <kirkmeyer03@gmail.com>
2024-05-21 14:04:54 -0700

    Update settings.py

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -126,7 +126,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 #Auth
 
-LOGIN_URL='/login/'
+LOGIN_URL = '/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
